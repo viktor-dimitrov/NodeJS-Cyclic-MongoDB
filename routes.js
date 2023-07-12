0bfa1abc5d58897fcbb8fc3a0fdb3d7fe87144eb
viktor-dimitrov <106183268+viktor-dimitrov@users.noreply.github.com>
2023-07-12 22:06:51 +0300

    update router

--- routes.js
+++ routes.js
@@ -6,6 +6,7 @@ const carController = require('./controllers/carController');
 
 router.use('/users', authController);
 router.use('/data/cars', carController);
+router.use('/data/records', recordController);
 router.use('/details/:_id', carController);
 
 module.exports = router;
