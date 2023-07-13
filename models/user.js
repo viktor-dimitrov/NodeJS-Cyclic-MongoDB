fb6bd9a5b1b192ca3223357c3444f54db9742a99
viktor-dimitrov <106183268+viktor-dimitrov@users.noreply.github.com>
2023-07-13 23:32:39 +0300

    add unique: true in userSchema

--- models/user.js
+++ models/user.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
+        unique:true,
         required: [true,'The username is required!'],
         minLength: [2, 'The username should be at least 2 characters long!'],
         maxLength: [10, 'The username should be 10 characters max long!']
